91363f4d8e2a33e84ef72b2bd9e62a99c9384e83
Ryqite <garbaruk.901@gmail.com>
2025-05-06 00:27:09 +0300

    all is working

--- app/src/main/java/com/example/variant1/MainActivity.kt
+++ app/src/main/java/com/example/variant1/MainActivity.kt
@@ -17,8 +17,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Divider
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,7 +30,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.startActivity
 import coil.compose.AsyncImage
 
 class MainActivity : ComponentActivity() {
@@ -143,7 +140,9 @@ fun ContactItem(contact: Contact,context: Context) {
             AsyncImage(
                 model = contact.photoUri,
                 contentDescription = "Contact photo",
-                placeholder = painterResource(R.drawable.ic_correct),
+                placeholder = painterResource(R.drawable.ic_default),
+                error = painterResource(R.drawable.ic_default),
+                fallback = painterResource(R.drawable.ic_default),
                 modifier = Modifier
                     .size(48.dp)
                     .clip(CircleShape)
